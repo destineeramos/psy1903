6421ad14a990b6d54f5598b4eca0bf60f291f7a2
Destinee Ramos <destineeramos@college.harvard.edu>
2025-09-16 16:34:55 -0400

    question 7 week 3 task set

--- web/week-3-task-set/app.js
+++ web/week-3-task-set/app.js
@@ -1,4 +1,40 @@
 //Identify elements on the page we will update 
+//let num1 = document.getElementById('num1');
+//let num2 = document.getElementById('num2');
+
+//generate random numbers to display on page load 
+//let randomNum1 = Math.floor(Math.random() * 10) + 1;
+//let randomNum2 = Math.floor(Math.random() * 10) + 1;
+
+//Update elements on the page
+//num1.innerHTML = randomNum1;
+//num2.innerHTML = randomNum2;
+
+
+//let response = prompt('What is your name?');
+//let count = response.length;
+//let firstLetter = response.charAt(0);
+//console.log(firstLetter);
+//let lastLetter = response.charAt(count - 1);
+//console.log(lastLetter);
+
+//let age = 20;
+//if (age = 25) {
+//console.log("Age is 25");
+//}
+
+//let a = 5;
+//let b = 10;
+//console.log(a > 3 && b < 15);
+
+//let over18 = false;
+//let hasGuardianApproval = true;
+//console.log(over18 || hasGuardianApproval); 
+
+//let count = 8;
+//console.log(count % 2 == 0);
+
+//Identify elements on the page we will update 
 let num1 = document.getElementById('num1');
 let num2 = document.getElementById('num2');
 
@@ -10,3 +46,17 @@ let randomNum2 = Math.floor(Math.random() * 10) + 1;
 num1.innerHTML = randomNum1;
 num2.innerHTML = randomNum2;
 
+//create the prompt for users 
+let response = prompt("What is " + randomNum1 + "+" + randomNum2 + '?');
+
+let feedback = '';
+
+if (response == randomNum1 + randomNum2) {
+    feedback = 'Correct!';
+} else if (response == (randomNum1 + randomNum2) + 1 || response == (randomNum1 + randomNum2) - 1) {
+    feedback = 'You were close!';
+} else {
+    feedback = 'Incorrect.';
+}
+//create the alert for users 
+alert(feedback + ' The expected answer is ' + (randomNum1 + randomNum2));
