9eb251fe6aac22ea415c6b74298526ed0810f9bc
Destinee Ramos <destineeramos@college.harvard.edu>
2025-09-23 16:38:29 -0400

    week 4 task set Q 2

--- web/week4-task-set/app.js
+++ web/week4-task-set/app.js
@@ -1,1 +1,8 @@
-console.log("hello")
+function celsiusToFahrenheit(celsius) {
+    let fahrenheit = (celsius * 1.8) + 32;
+    return fahrenheit;
+}
+console.log(celsiusToFahrenheit(10)); // Expected output: 50
+console.log(celsiusToFahrenheit(-5)); // Expected output: 23
+
+
